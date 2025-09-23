b633584b5e5c730577a9aef2958a19217390aa37
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-09-23 22:33:57 +0200

    Overflow protection in condition_variable.wait_for.

--- src/roo_threads/impl/cppstd/condition_variable.h
+++ src/roo_threads/impl/cppstd/condition_variable.h
@@ -5,10 +5,10 @@
 
 #ifdef ROO_THREADS_USE_CPPSTD
 
-#include "roo_threads/impl/cppstd/mutex.h"
-
 #include <chrono>
 #include <condition_variable>
+
+#include "roo_threads/impl/cppstd/mutex.h"
 
 namespace roo_threads {
 namespace cppstd {
@@ -47,6 +47,15 @@ class condition_variable {
 
   cv_status wait_for(unique_lock<mutex>& lock,
                      const roo_time::Interval& duration) {
+    static constexpr int64_t kMaxSafeWaitMicros = 10LL * 24 * 3600 * 1000000;
+    if (duration.inMicros() >= kMaxSafeWaitMicros) {
+      // Protecting against overflow, e.g. wait_for(roo_time::Interval::Max()):
+      // never wait for longer than 10 days; spuriously wake up if needed.
+      // Using safely low max duration of 10 days, as ESP32 seems to overflow at
+      // about 24 days.
+      cond_.wait_for(lock.lock_, std::chrono::microseconds(kMaxSafeWaitMicros));
+      return cv_status::no_timeout;
+    }
     return cond_.wait_for(lock.lock_,
                           std::chrono::microseconds(duration.inMicros())) ==
                    std::cv_status::no_timeout
@@ -57,8 +66,16 @@ class condition_variable {
   template <typename Predicate>
   bool wait_for(unique_lock<mutex>& lock, const roo_time::Interval& duration,
                 Predicate p) {
-    return cond_.wait_for(lock.lock_,
-                          std::chrono::microseconds(duration.inMicros()), p);
+    // Protecting against overflow, e.g. wait_for(roo_time::Interval::Max()):
+    // never wait for longer than 10 days; spuriously wake up if needed.
+    // Using safely low max duration of 10 days, as ESP32 seems to overflow at
+    // about 24 days.
+    static constexpr int64_t kMaxSafeWaitMicros = 10LL * 24 * 3600 * 1000000;
+    int64_t wait_micros = duration.inMicros() < kMaxSafeWaitMicros
+                              ? duration.inMicros()
+                              : kMaxSafeWaitMicros;
+    return cond_.wait_for(lock.lock_, std::chrono::microseconds(wait_micros),
+                          p);
   }
 
  private:
