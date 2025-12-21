ae22f87c6edb6ecf7978ded67f9045d6ffd52d9b
Dawid Kurzyniec <dawid.kurzyniec@gmail.com>
2025-12-21 20:35:23 +0100

    Use freertos threads on ESP32, to have control over thread attributes (stack size, etc.)

--- src/roo_threads/thread.h
+++ src/roo_threads/thread.h
@@ -2,28 +2,30 @@
 
 #include "roo_threads/impl/resolve.h"
 
-#if (defined ROO_THREADS_USE_ESP32)
+#if (defined ROO_THREADS_USE_FREERTOS)
 
-#include "roo_threads/impl/esp32/thread.h"
+// If both cppstd and freertos are available (e.g. on ESP32), we prefer
+// freertos, because it offers more control over thread attributes.
+
+#include "roo_threads/impl/freertos/thread.h"
 
 namespace roo {
 
-using thread = roo_threads::esp32::thread;
-
+using thread = ::roo_threads::freertos::thread;
 namespace this_thread {
 
 inline thread::id get_id() noexcept {
-  return roo_threads::esp32::this_thread::get_id();
+  return roo_threads::freertos::this_thread::get_id();
 }
 
-inline void yield() noexcept { roo_threads::esp32::this_thread::yield(); }
+inline void yield() noexcept { roo_threads::freertos::this_thread::yield(); }
 
 inline void sleep_for(const roo_time::Duration& duration) {
-  roo_threads::esp32::this_thread::sleep_for(duration);
+  roo_threads::freertos::this_thread::sleep_for(duration);
 }
 
 inline void sleep_until(const roo_time::Uptime& when) {
-  roo_threads::esp32::this_thread::sleep_until(when);
+  roo_threads::freertos::this_thread::sleep_until(when);
 }
 
 }  // namespace this_thread
@@ -57,34 +59,6 @@ inline void sleep_until(const roo_time::Uptime& when) {
 }  // namespace this_thread
 
 }  // namespace roo
-
-#elif (defined ROO_THREADS_USE_FREERTOS)
-
-#include "roo_threads/impl/freertos/thread.h"
-
-namespace roo {
-
-using thread = ::roo_threads::freertos::thread;
-namespace this_thread {
-
-inline thread::id get_id() noexcept {
-  return roo_threads::freertos::this_thread::get_id();
-}
-
-inline void yield() noexcept { roo_threads::freertos::this_thread::yield(); }
-
-inline void sleep_for(const roo_time::Duration& duration) {
-  roo_threads::freertos::this_thread::sleep_for(duration);
-}
-
-inline void sleep_until(const roo_time::Uptime& when) {
-  roo_threads::freertos::this_thread::sleep_until(when);
-}
-
-}  // namespace this_thread
-
-}  // namespace roo
-
 
 #elif (defined ROO_THREADS_USE_ROO_TESTING)
 
